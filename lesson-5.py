ac12c6fa01c1038dee4a7daf9ebd4d11726f179c
rallegit <rallegit@gmail.com>
2025-04-13 23:21:08 +0700

    lesson5 weather near me

--- lesson-5.py
+++ lesson-5.py
@@ -7,18 +7,25 @@ client = OpenAI()
 input_messages = [
     {
         "role":"user",
-        "content":"What is the current weather in London?"
+        "content":"What is the current weather near me?"
     }
 ]
 
 tools = [
     {
-        "type": "web_search_preview"
+        "type": "web_search_preview",
+        "user_location": {
+            "type": "approximate",
+            "country": "GB",
+            "city": "London",
+            "region": "London"
+        }
     }
 ]
 
 response = client.responses.create(
     model="gpt-4o-mini",
+    instructions="When looking up real time data using web search, do not ask for the user's location. It will be provided by the web search tool itself.",
     input=input_messages,
     tools=tools
 )
