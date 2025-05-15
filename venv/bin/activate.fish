52cd6d2dc00d7fe12282f96526644794852f66fb
Ralf Gaertig <rallegit@gmail.com>
2025-05-16 00:12:34 +0200

    updated venv after recovery

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rglinux/.git/responseapi-tutorial/venv
+set -gx VIRTUAL_ENV /home/rglinux/prj/responsesapi-tutorial/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
